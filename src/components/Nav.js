5dc53ee3dc94ceaa7180bde3f7aeb359c7a41756
skboyle <boyle.steven@gmail.com>
2018-01-07 18:36:17 -0500

    Text Adjustment

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,7 +10,7 @@ const Nav = ({ name }) => {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-white">
-        <a className="navbar-brand">S. Boyle</a>
+        <a className="navbar-brand">S. K. Boyle</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>

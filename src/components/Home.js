5d7314d5b2cb9ae670a641637107b6e6e03f680f
skboyle <boyle.steven@gmail.com>
2018-01-08 11:32:08 -0500

    Typo

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ const Home = ({ name }) => {
       <Nav/>
       <div className="container">
         <div className="home-greeting">
-          <h3>Hi, I`m a web developer!</h3>
+          <h3>Hi, {`I\'m`} a web developer!</h3>
           <h4>I like making fun, intuitive and user friendly websites. I have experience with Ruby, Rails, Sinatra, Javacsript, React, Node, SQL, Bootstrap, Foundation and SASS. Click though my portfolio to see my work. </h4>
           <div className="div1" id="one"></div>
           <div className="div1" id="two"></div>

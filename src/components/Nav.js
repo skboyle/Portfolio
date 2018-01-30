8c74b326b8d79a128047ba4530d40f2116c40549
skboyle <boyle.steven@gmail.com>
2018-01-30 01:25:13 -0500

    New Nac

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -9,28 +9,20 @@ import '../assets/styles/App.scss';
 const Nav = ({ name }) => {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-white">
-        <a className="navbar-brand">S. K. Boyle</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav">
-            <Link to='/' className="nav-item nav-link">Home</Link>
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Portfolio
-              </a>
-              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <Link to='/feedbackloop' className="nav-item nav-link">Feedback Loop</Link>
-                <Link to='/topmountains' className="nav-item nav-link">Top Mountains</Link>
-              </div>
+      <ul className="topnav">
+            <li>
+              <Link to='/' className="">SKB</Link>
             </li>
-
-            <Link to='/contact' className="nav-item nav-link">Contact</Link>
-          </div>
-        </div>
-      </nav>
+            <li>
+              <Link to='/feedbackloop' className="">FEEDBACK LOOP</Link>
+            </li>
+            <li>
+              <Link to='/topmountains' className="">TOP MOUNTAINS</Link>
+            </li>
+            <li>
+            <Link to='/contact' className="">CONTACT</Link>
+          </li>
+      </ul>
     </div>
   );
 };
